f3a8b7b37fafefb6184fbc8507f967bd91ab19cb
Maria Cecilia <mclemos65@gmail.com>
2023-06-22 16:18:04 -0300

    lançando projeto

---
+++ to-do/src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about pa-6">
+    <h1>Abaut</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum error, impedit fugit quos mollitia at similique doloribus, maxime hic beatae dicta corporis unde veniam aliquid excepturi ratione? Qui, ullam laudantium!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum error, impedit fugit quos mollitia at similique doloribus, maxime hic beatae dicta corporis unde veniam aliquid excepturi ratione? Qui, ullam laudantium!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum error, impedit fugit quos mollitia at similique doloribus, maxime hic beatae dicta corporis unde veniam aliquid excepturi ratione? Qui, ullam laudantium!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum error, impedit fugit quos mollitia at similique doloribus, maxime hic beatae dicta corporis unde veniam aliquid excepturi ratione? Qui, ullam laudantium!</p>
+  </div>
+</template>
